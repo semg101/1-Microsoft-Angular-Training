16d9dab5ab179d2a0e1b47f724df36a75b3fe68f
semg101 <gansosaker@yahoo.fr>
2020-05-14 15:38:19 +0100

    1 Microsoft Angular Training

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { GitSearchService } from './git-search.service';
+import { GitSearchComponent } from './git-search/git-search.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', 
+    component: HomePageComponent 
+  },
+  { path: 'search',      
+    redirectTo: '/search/angular', 
+    pathMatch: 'full' },
+  {
+    path: 'search/:query',
+    component: GitSearchComponent,
+    data: {title: 'Git Search'}
+  },
+  { path: '**', component: NotFoundComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GitSearchComponent,
+    HomePageComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
+  ],
+  providers: [GitSearchService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
